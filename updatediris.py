d60bdab83b048765b9c8ff1b87a0e798f670d493
shubhamg-2710 <shubhamgaikwad2710@gmail.com>
2024-12-13 12:33:18 +0530

    .py file update

--- updatediris.py
+++ updatediris.py
@@ -1,6 +1,2 @@
-from sklearn.datasets import datasets
-print("Error bhari jindagi hey bhai 2 bar!")
-print("Error bhari jindagi hey bhai 3 bar!")
-
-print("\n\n\n\n\n\n")
+print("Welcome to GitHub")
 
